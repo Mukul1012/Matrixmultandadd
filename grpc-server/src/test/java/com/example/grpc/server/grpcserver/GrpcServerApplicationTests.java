65bdf91086a58119c2075a7cb8564a3d58b8a27d
Mukul1012 <mukul.veerkar@gmail.com>
2022-04-19 04:47:27 +0100

    Commit

---
+++ grpc-server/src/test/java/com/example/grpc/server/grpcserver/GrpcServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.grpc.server.grpcserver;
+
+import org.junit;
+import org.junit.runner.RunWith;
+import org.springframework;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrpcServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
